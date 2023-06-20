4eaa4320718cbb0da91d9ad996b40acebc287de0
DevSamuelNwankwo <73386074+DevSamuelNwankwo@users.noreply.github.com>
2023-06-20 14:56:30 +0100

    learning the basics like functions and value types

---
+++ learnsolidity day1/basic.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity <0.8.20;
+
+// contract learnsolidity {
+    // testunderflow does no longer work
+//     //  function testUnderFlow () public view returns (uint) {
+//     //       uint x = 0;
+//     //           x
+//     //       return x ;
+//     //  }
+
+//      function testuUncheckedOverflow() public pure returns (uint ) {
+//          uint x = 0;
+//          unchecked {x--;}
+//          return x;
+//      }
+
+// }
+// my hello world contract
+
+// contract helloworld {
+
+//   string public myString = "hello world !";
+
+// }
+
+
+
+
+
+// value types in solidity
+// contract ValueTypes {
+//     bool public b = true ;
+//     uint public c = 123;
+//     int public a = -4;
+//     int public minHint = type(int).min;
+//     int public maxHint = type(int).max;
+//     address public addr = 0x95CdddB13c6525D6dB5449D694E03380CE630Ad2;
+//     bytes32 public b32 = 0x95CdddB13c6525D6dB5449D694E03380CE630Ad20x95CdddB13c6525D6dB5449D694E03380CE630Ad20x95CdddB13c6525D6dB5449D694E03380CE630Ad20x95CdddB13c6525D;
+// //  you will see bytes 32 when working with cryptographic hash functions 
+// }
+
+
+
+// learn about function in soliditiy
+// contract learnfunctions {
+//     function add(uint a, uint b) public pure returns (uint) {
+//          return a +b;
+//     }
+
+//     function sub(uint a, uint b) public pure returns (uint ) {
+//          return a - b;
+//     }
+// }
+
+// custom errors,
+
+
+// there are three types of variables in solidity 
+
+
+// default values in solidity 
+// contract defaultvalues {
+//     bool public a = true ;
+//     uint public b ;
+//     int public c ;
+//     address public adr ;
+//     bytes32 public b32 ; 
+// }
+
+
+// constants in solidity
+// contract constants {
+//     address public constant then with caps
+// }
+
+
+
+// contract ifElse {
+//     function ifElseKids (uint _x)public pure returns (uint) {
+//         if(_x <10 ) {
+//            return 3 ;
+//         }else {
+//             return 0;
+//         }
+
+//     }
+// }
+
+// ways of handling errors 
+// require, assert(condition);, revert();
+// contract testRequire 
